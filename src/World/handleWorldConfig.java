3c230f6d923b11512cfe6ba7045e85550a477292
flips30240 <flips92691@gmail.com>
2013-10-18 00:49:11 -0700

    creates World.properties if one is not present
    also creates world.properties if config folder is empty

--- src/World/handleWorldConfig.java
+++ src/World/handleWorldConfig.java
@@ -1,6 +1,9 @@
 package World;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class handleWorldConfig {
     
@@ -17,18 +20,43 @@ public class handleWorldConfig {
         File [] listOfFiles = folder.listFiles();
         
         //check for world.properties file
-        for (int i = 0; i < listOfFiles.length; i++){
-            if (listOfFiles[i].isFile()){
-                files = listOfFiles[i].getName();
-                if (files.equals("World.properties")){
-                    System.out.println("Found World Config File");
-                }
-                else{
-                    //create a world.properties file
+        if (listOfFiles.length == 0){
+            //create a world.properties file
                     System.out.println("No World Config File:"
                             + " Creating one now");
-                }
-            }
+                    this.createPropertiesFile();
     }
+        else{
+            for (int i = 0; i < listOfFiles.length; i++){
+                if (listOfFiles[i].isFile()){
+                    files = listOfFiles[i].getName();
+
+                    if (files.equals("World.properties")){
+                        System.out.println("Found World Config File");
+                    }
+                    else{
+                        //create a world.properties file
+                        System.out.println("No World Config File:"
+                                + " Creating one now");
+                        this.createPropertiesFile();
+                    }
+                }
+        }
+    }
+    }
+    
+    public void createPropertiesFile(){
+        Properties props = new Properties();
+        try{
+            props.setProperty("sizeX", "100");
+            props.setProperty("sizeY", "50");
+            props.setProperty("sizeZ", "25");
+            
+            props.store(new FileOutputStream("./assets/Config/"
+                    + "World.properties"), null);
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
     }
 }
